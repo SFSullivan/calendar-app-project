46cd60b95106f0b605372d2ed858e1a1b8dd2efc
SSullivan <spencersullivan1990@gmail.com>
2022-07-05 16:45:09 -0500

    timeblocks and some variables established

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+
+var sched0900 = $("#0900")
+var sched1000 = $("#1000")
+var sched1100 = $("#1100")
+var sched1200 = $("#1200")
+var sched1300 = $("#1300")
+var sched1400 = $("#1400")
+var sched1500 = $("#1500")
+var sched1600 = $("#1600")
+var sched1500 = $("#1500")
+var containEl = $(".container")
+var saveBtn = $(".save-icon")
+
+let scheduleAray = [
+    sched0900,
+    sched1000,
+    sched1100,
+    sched1200,
+    sched1300,
+    sched1400,
+    sched1500,
+    sched1600,
+    sched1700
+];
